2d20e56fc17ef96b3f4b0ce71261a9537bab7353
Murli <murlimanohartripathy1997@gmail.com>
2022-12-08 17:04:41 +0530

    http and making firebase server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,13 +9,16 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'form';
-
+  constructor(private httpClient: HttpClient) { }
   loginuserform = new FormGroup({
-    username: new FormControl(null,[Validators.required,Validators.email]),
-    password: new FormControl(null,[Validators.required,Validators.minLength(6)])
+    username: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)])
   })
-  userlogin(){
-    console.log(this.loginuserform.value)
+  userlogin() {
+    let messageData = { username: this.loginuserform.get('username')?.value, password: this.loginuserform.get('password')?.value }
+    this.httpClient.post('https://angular-backend-6947d-default-rtdb.firebaseio.com/messages.json', messageData).subscribe((e:any)=>{
+      console.log(e)
+    })
     this.loginuserform.reset()
   }
 }
